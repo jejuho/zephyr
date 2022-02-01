01c99a582bc3a61abeadffb93c295efc4ce046dc
Juho Eskeli <juho.eskeli@gmail.com>
2022-02-01 23:38:41 +0200

    Testing with 3 DS18b20 sensors connected to a single 1-wire bus.

--- samples/sensor/ds18b20/src/main.c
+++ samples/sensor/ds18b20/src/main.c
@@ -9,18 +9,87 @@
 #include <devicetree.h>
 #include <drivers/sensor.h>
 
+#if 1
+
+struct ds18b20_scratchpad {
+	int16_t temp;
+	uint8_t alarm_temp_high;
+	uint8_t alarm_temp_low;
+	uint8_t config;
+	uint8_t res[3];
+	uint8_t crc;
+};
+
+struct ds18b20_config {
+	const struct device *bus;
+	uint8_t family;
+	uint8_t resolution;
+};
+
+struct ds18b20_data {
+	uint8_t rom[8];
+	struct ds18b20_scratchpad scratchpad;
+	uint8_t power_supply;
+	bool lazy_loaded;
+};
+#endif
+
+#define DS_1 DT_INST(0, maxim_ds18b20)
+#define DS_2 DT_INST(1, maxim_ds18b20)
+#define DS_3 DT_INST(2, maxim_ds18b20)
 /*
  * Get a device structure from a devicetree node with compatible
  * "maxim,ds18b20". (If there are multiple, just pick one.)
  */
-static const struct device *get_ds18b20_device(void)
+static const struct device *get_ds18b20_device(int id)
 {
-	const struct device *dev = DEVICE_DT_GET_ANY(maxim_ds18b20);
+	struct device *dev = 0;
+	if(id == 0) {
+		dev = DEVICE_DT_GET(DS_1);
+	}
+	else if(id == 1) {
+		dev = DEVICE_DT_GET(DS_2);
+	} else
+	if(id == 2) {
+		dev = DEVICE_DT_GET(DS_3);
+	}
 
 	if (dev == NULL) {
 		/* No such node, or the node does not have status "okay". */
 		printk("\nError: no device found.\n");
 		return NULL;
+	}
+
+	struct ds18b20_data *data = dev->data;
+	if(id == 0) {
+		data->rom[0] = 0x28;
+		data->rom[1] = 0xA0;
+		data->rom[2] = 0x60;
+		data->rom[3] = 0x86;
+		data->rom[4] = 0x04;
+		data->rom[5] = 0x00;
+		data->rom[6] = 0x00;
+		data->rom[7] = 0x1d;
+	} else
+	if(id == 1) {
+		data->rom[0] = 0x28;
+		data->rom[1] = 0x46;
+		data->rom[2] = 0x91;
+		data->rom[3] = 0x86;
+		data->rom[4] = 0x04;
+		data->rom[5] = 0x00;
+		data->rom[6] = 0x00;
+		data->rom[7] = 0xed;
+	} else
+	if(id == 2) {
+		data->rom[0] = 0x28;
+		data->rom[1] = 0x0f;
+		data->rom[2] = 0x28;
+		data->rom[3] = 0x87;
+		data->rom[4] = 0x04;
+		data->rom[5] = 0x00;
+		data->rom[6] = 0x00;
+		data->rom[7] = 0x3d;
 	}
 
 	if (!device_is_ready(dev)) {
@@ -36,7 +105,9 @@ static const struct device *get_ds18b20_device(void)
 
 void main(void)
 {
-	const struct device *dev = get_ds18b20_device();
+	const struct device *dev = get_ds18b20_device(0);
+	const struct device *dev2 = get_ds18b20_device(1);
+	const struct device *dev3 = get_ds18b20_device(2);
 
 	if (dev == NULL) {
 		return;
@@ -47,8 +118,14 @@ void main(void)
 
 		sensor_sample_fetch(dev);
 		sensor_channel_get(dev, SENSOR_CHAN_AMBIENT_TEMP, &temp);
+		printk("Dev Temp: %d.%06d\n", temp.val1, temp.val2);
+		sensor_sample_fetch(dev2);
+		sensor_channel_get(dev2, SENSOR_CHAN_AMBIENT_TEMP, &temp);
+		printk("Dev2 Temp: %d.%06d\n", temp.val1, temp.val2);
+		sensor_sample_fetch(dev3);
+		sensor_channel_get(dev3, SENSOR_CHAN_AMBIENT_TEMP, &temp);
+		printk("Dev3 Temp: %d.%06d\n", temp.val1, temp.val2);
 
-		printk("Temp: %d.%06d\n", temp.val1, temp.val2);
 		k_sleep(K_MSEC(5000));
 	}
 }
